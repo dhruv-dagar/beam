13166b7b234c2aa48832a47602790b8221221ec2
Dhruv Dagar <dhruvdagar05@gmail.com>
2026-09-11 13:17:51 +0530

    [BEAM-39684] Define portable logical type contract

---
+++ sdks/go/pkg/beam/core/runtime/graphx/schema/portable_logicaltypes.go
@@ -0,0 +1,35 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schema
+
+import (
+	"reflect"
+
+	pipepb "github.com/apache/beam/sdks/v2/go/pkg/beam/model/pipeline_v1"
+)
+
+// PortableLogicalType describes a Beam logical type using its portable URN.
+//
+// Unlike the legacy Go LogicalType, a portable logical type is identified by
+// the Beam URN that appears on the wire. Representation and argument metadata
+// are supplied as pipeline schema protos so they can be preserved exactly.
+type PortableLogicalType interface {
+	URN() string
+	GoType() reflect.Type
+	Representation() *pipepb.FieldType
+	ArgumentType() *pipepb.FieldType
+	Argument() *pipepb.FieldValue
+}
